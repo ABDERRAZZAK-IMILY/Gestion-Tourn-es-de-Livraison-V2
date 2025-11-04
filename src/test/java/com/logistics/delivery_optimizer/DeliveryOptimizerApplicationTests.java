93fbd40cb5caec1fa8e8e082caa69a56aaef5613
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-11-04 16:06:54 +0100

    Feat : enable 'dev' profile for DeliveryOptimizerApplicationTests

--- src/test/java/com/logistics/delivery_optimizer/DeliveryOptimizerApplicationTests.java
+++ src/test/java/com/logistics/delivery_optimizer/DeliveryOptimizerApplicationTests.java
@@ -2,8 +2,10 @@ package com.logistics.delivery_optimizer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class DeliveryOptimizerApplicationTests {
 
 	@Test
