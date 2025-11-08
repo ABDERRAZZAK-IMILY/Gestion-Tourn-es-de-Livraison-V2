2f82434fdeceb0da6afdb0ac25ab4d1cd4970c23
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-11-08 09:40:37 +0100

    Feat : modify DeliveryRepository to return list of deliveries by status instead of paginated results

--- src/main/java/com/logistics/delivery_optimizer/repository/DeliveryRepository.java
+++ src/main/java/com/logistics/delivery_optimizer/repository/DeliveryRepository.java
@@ -8,12 +8,9 @@ import com.logistics.delivery_optimizer.Model.Delivery;
 import com.logistics.delivery_optimizer.Model.Enums.DeliveryStatus;
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
 
-    Page<Delivery> findByStatus(DeliveryStatus status, Pageable pageable);
+    List<Delivery> findByStatus(DeliveryStatus status);
     List<Delivery> findByTourId(Long tourId);
 
     @Query(value = "SELECT * From Delivery Where Status = :status" , nativeQuery = true)
