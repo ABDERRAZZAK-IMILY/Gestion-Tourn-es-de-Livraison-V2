213ad989ba6d72f77de48999d70b61a89e1bb597
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-11-08 10:53:40 +0100

    Feat : add validation annotations to CustomerRequestDTO fields

--- src/main/java/com/logistics/delivery_optimizer/dto/CustomerRequestDTO.java
+++ src/main/java/com/logistics/delivery_optimizer/dto/CustomerRequestDTO.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalTime;
 
@@ -14,11 +17,17 @@ import java.time.LocalTime;
 @NoArgsConstructor
 @Builder
 public class CustomerRequestDTO {
-
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @NotBlank(message = "Address cannot be blank")
     private String address;
+    @NotNull(message = "Latitude cannot be null")
+    @Range(min = -90, max = 90, message = "Latitude must be between -90 and 90")
     private double latitude;
+    @NotNull(message = "Longitude cannot be null")
+    @Range(min = -180, max = 180, message = "Longitude must be between -180 and 180")
     private double longitude;
+
     private String preferredTimeSlot;
 
 }
